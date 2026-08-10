4db295f647dfb607a314adc966e49275c623a157
andrea8980 <andrea@casaloda.it>
2026-08-10 21:18:12 +0200

    fix: update navbar links to use route helpers for better maintainability

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav style="background:#3498db; padding:15px;">
-    <a href="/" class="link" style="color:white; ; font-weight:600;">Home</a>
-    <a href="/second-page" class="link">Pagina 2</a>
-    <a href="/third-page" class="link">Pagina 3</a>
-    <a href="/fourth-page" class="link">Pagina 4</a>
-    <a href="/fifth-page" class="link">Pagina 5</a>
+    <a href="{{ route('home') }}" class="link">Home</a>
+    <a href="{{ route('second-page') }}" class="link">Pagina 2</a>
+    <a href="{{ route('third-page') }}" class="link">Pagina 3</a>
+    <a href="{{ route('fourth-page') }}" class="link">Pagina 4</a>
+    <a href="{{ route('fifth-page') }}" class="link">Pagina 5</a>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -7,27 +7,26 @@ Route::get('/', function () {
 $informazioni = "Questa è la prima pagina del sito, benvenuto!";
 
     return view('home', compact('informazioni'));
-});
+}) -> name('home');
 
 Route::get('/second-page', function () {
 
 $informazioni = "Questa è la seconda pagina del sito, benvenuto!";
     return view('second-page', compact('informazioni'));
-});
+}) -> name('second-page');
 
 Route::get('/third-page', function () {
     $informazioni = "Questa è la terza pagina del sito, benvenuto!";
     return view('third-page', compact('informazioni'));
-});
+}) -> name('third-page');
 
 Route::get('/fourth-page', function () {
     $informazioni = "Questa è la quarta pagina del sito, benvenuto!";
     return view('fourth-page', compact('informazioni'));
-});
+}) -> name('fourth-page');
 
 Route::get('/fifth-page', function () {
     $informazioni = "Questa è la quinta pagina del sito, benvenuto!";
     return view('fifth-page', compact('informazioni'));
-});
+}) -> name('fifth-page');
 
-?>
