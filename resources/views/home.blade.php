7b502e107b06f87391a94fe9861da559e302f233
andrea8980 <andrea@casaloda.it>
2026-08-10 19:10:04 +0200

    set up vite and css stylesheet.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <h1>Questa è la mia prima homepage PHP!</h1>
-    <a href="second-page">Vai alla seconda pagina</a>
+        <div style="text-align:center;">
+        <a href="/second-page" class="btn-test">Vai alla pagina 2</a>
+    </div>
 </body>
+
 </html>
--- resources/views/second-page.blade.php
+++ resources/views/second-page.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seconda Pagina</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <h1>Questa è la mia seconda pagina PHP!</h1>
-    <a href="/">Torna alla homepage</a>
+        <div style="text-align:center;">
+        <a href="/" class="btn-test">Torna alla homepage</a>
+    </div>
 </body>
+
 </html>
